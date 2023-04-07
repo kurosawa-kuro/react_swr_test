8b2b48cd89a7c473993dccc376454657f8e6edb9
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-04-08 01:03:18 +0900

    swr test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import useSWR from 'swr';
+import axios from 'axios';
+
+export const fetcher = (url) => axios.get(url).then((res) => res.data);
 
 function App() {
+  const { data: items, error } = useSWR('http://localhost:3000/items', fetcher);
+
+  if (error) {
+    return <div>Error loading data</div>;
+  }
+
+  if (!items) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {items.map(item => (
+        <div key={item.item_id} className="item">
+          <h2>{item.item_name}</h2>
+          <img src={item.item_image_url} alt={item.item_name} />
+          <p>Created at: {item.created_at}</p>
+          <h3>By {item.user_name}</h3>
+        </div>
+      ))}
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,72 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import axios from 'axios';
+import useSWR from 'swr';
+import App, { fetcher } from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock('axios');
+jest.mock('swr');
+
+describe('App component', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  test('displays error message when data fails to load', async () => {
+    useSWR.mockReturnValue({
+      data: null,
+      error: true,
+    });
+
+    render(<App />);
+    expect(screen.getByText('Error loading data')).toBeInTheDocument();
+  });
+
+  test('displays loading message when data is being fetched', () => {
+    useSWR.mockReturnValue({
+      data: null,
+      error: null,
+    });
+
+    render(<App />);
+    expect(screen.getByText('Loading...')).toBeInTheDocument();
+  });
+
+  test('displays fetched data correctly', async () => {
+    const testData = [
+      {
+        item_id: 1,
+        item_name: 'Item 1',
+        item_image_url: 'http://example.com/image.jpg',
+        created_at: '2021-01-01',
+        user_name: 'User 1',
+      },
+    ];
+
+    axios.get.mockResolvedValueOnce({ data: testData });
+
+    useSWR.mockReturnValue({
+      data: testData,
+      error: null,
+    });
+
+    render(<App />);
+    await waitFor(() => {
+      expect(screen.getByText('Item 1')).toBeInTheDocument();
+      expect(screen.getByText('Created at: 2021-01-01')).toBeInTheDocument();
+      expect(screen.getByText('By User 1')).toBeInTheDocument();
+    });
+  });
+
+  test('fetcher function returns data from the server', async () => {
+    const testData = [{ id: 1, name: 'Test' }];
+
+    axios.get.mockResolvedValueOnce({ data: testData });
+
+    const result = await fetcher('/test');
+
+    expect(result).toEqual(testData);
+    expect(axios.get).toHaveBeenCalledWith('/test');
+  });
 });
